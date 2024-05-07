3d007c94e5497c5d778f0c282e50df5c7bd5de91
iYoonJongWon <gksso25@gmail.com>
2024-05-07 17:12:27 +0900

    Update assign0507.kt

--- assign0507.kt
+++ assign0507.kt
@@ -1,1 +1,14 @@
+//FizzBuzz 구현
+for (i in 1..100){
+    if (i % 15 == 0){
+        println("PizzBuzz")
+    }
+        else if (i % 3 == 0){
+        println("Pizz")
+    }
+        else if (i % 5 == 0){
+        println("Buzz")
+    }
+    else println(i)
+}
 
